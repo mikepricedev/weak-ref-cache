ef7d40044e7612dfaeb0ecdc1a41cbf43376495e
Michael Price <mikepricedev@gmail.com>
2023-11-08 17:55:10 -0600

    feat: adds cjs build step

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
-export declare function getWeakRef<Obj extends object>(obj: Obj): WeakRef<Obj>;
-
+/**
+ * Retrieve or create a weak reference for the given object.
+ *
+ * @template {object} Obj
+ * @param {Obj} obj
+ * @returns {WeakRef<Obj>}
+ */
+export function getWeakRef<Obj extends object>(obj: Obj): WeakRef<Obj>;
 export default getWeakRef;
